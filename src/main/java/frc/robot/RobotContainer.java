b3093dee643e70053efe6c3e45e68485111e3044
Edwin Yazbec <edwin.yazbec@gmail.com>
2026-02-22 12:47:53 -0800

    better buton bindings?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,6 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -27,11 +26,11 @@ public class RobotContainer {
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.Velocity); // Use open-loop control for drive motors
+    private final SwerveRequest.FieldCentric fieldCentriceDrive = new SwerveRequest.FieldCentric()
+        .withDeadband(MaxSpeed * 0.1)
+        .withRotationalDeadband(MaxAngularRate * 0.1)
+        .withDriveRequestType(DriveRequestType.Velocity);
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
@@ -43,7 +42,6 @@ public class RobotContainer {
     private final Launcher launcher = new Launcher();
     private final Intake   intake   = new Intake();
     private final Indexer  indexer  = new Indexer();
-
 
 
     public RobotContainer() {
@@ -64,9 +62,10 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-driverController.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-driverController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-driverController.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+                fieldCentriceDrive
+                    .withVelocityX(-driverController.getLeftY() * MaxSpeed)
+                    .withVelocityY(-driverController.getLeftX() * MaxSpeed)
+                    .withRotationalRate(-driverController.getRightX() * MaxAngularRate)
             )
         );
 
@@ -78,9 +77,6 @@ public class RobotContainer {
         );
 
         driverController.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        driverController.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))
-        ));
 
         // // Run SysId routines when holding back/start and X/Y.
         // // Note that each routine should be run exactly once in a single log.
@@ -94,56 +90,55 @@ public class RobotContainer {
 
         drivetrain.registerTelemetry(logger::telemeterize);
 
-        // launcher stuff
-        driverController.rightTrigger(0.5)
-            .whileTrue(
-                new InstantCommand(() -> indexer.setRollerSpeed(0.6))
-                .andThen(new InstantCommand(() -> indexer.setKickerspeed(-0.3)))
-            )
-            .onFalse(
-                // new InstantCommand(() -> launcher.setFlywheelPercent(0.0))
-                new InstantCommand(() -> indexer.setRollerSpeed(0.0))
-                .andThen(new InstantCommand(() -> indexer.setKickerspeed(0.0)))
-            );
-        
-        driverController.a().onTrue(new InstantCommand(() -> launcher.setFlywheelPercent(0.5)));
-        driverController.b().onTrue(new InstantCommand(() -> launcher.setFlywheelPercent(0)));
-
-        // intake stuff
-        driverController.rightBumper()
-            .onTrue(new InstantCommand(() -> intake.setPosition(3.9)))
-            .onFalse(new InstantCommand(() -> intake.setPosition(0)));
-
+        // Intake control
         driverController.leftTrigger(0.5)
-            .onTrue(new InstantCommand(() -> intake.setRollerSpeed(0.6)))
-            .onFalse(new InstantCommand(() -> intake.setRollerSpeed(0)));
+            .onTrue(// deploy intake
+                new InstantCommand(() -> intake.setPosition(3.9))
+            )
+            .whileTrue(// run intake
+                new InstantCommand(() -> intake.setRollerSpeed(0.6)).andThen(
+                )
+            )
+            .onFalse(// turn off intake
+                new InstantCommand(() -> intake.setRollerSpeed(0.0)).andThen(
+                new InstantCommand(() -> indexer.setRollerSpeed(0.3))
+            )
+        );
 
-        driverController.povUp().onTrue(new InstantCommand(()   -> launcher.setHoodPosition(3)));
-        driverController.povDown().onTrue(new InstantCommand(() -> launcher.setHoodPosition(0)));
-        // kicker code
-        // kicker motor should operate when we have fuel, and not when we don't
+        driverController.leftBumper().onTrue(new InstantCommand(() -> intake.setPosition(0)));
         
+        // launcher control
+        // tap trigger past 50% to turn on launcher
+        driverController.rightTrigger(0.5).debounce(0.1)
+        .onTrue(// turn on launcher, hood to angle
+            new InstantCommand(() -> launcher.setHoodPosition(3)).andThen(
+            new InstantCommand(() -> launcher.setFlywheelPercent(0.6))
+            )
+        );
 
-        
+        // hold trigger all the way for 0.5 seconds to launch stuff
+        driverController.leftTrigger(1).debounce(0.5)
+            .whileTrue(// turn on feeder
+                new InstantCommand(() -> indexer.setKickerspeed(-0.3)).andThen(
+                new InstantCommand(() -> indexer.setRollerSpeed(0.6))
+                )
+            )
+            .onFalse(// turn off feeder
+                new InstantCommand(() -> indexer.setKickerspeed(0)).andThen(
+                new InstantCommand(() -> indexer.setRollerSpeed(0))
+                )
+            );
+
+        // right bumber turns off launcher
+        driverController.rightBumper()
+            .onTrue(// turn off launcher, move hood back
+                new InstantCommand(() -> launcher.setHoodPosition(0)).andThen(
+                new InstantCommand(() -> launcher.setFlywheelPercent(0))
+                )
+            );
     }
 
     public Command getAutonomousCommand() {
-        // Simple drive forward auton
-        final var idle = new SwerveRequest.Idle();
-        // return Commands.sequence(
-        //     // Reset our field centric heading to match the robot
-        //     // facing away from our alliance station wall (0 deg).
-        //     drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
-        //     // Then slowly drive forward (away from us) for 5 seconds.
-        //     drivetrain.applyRequest(() ->
-        //         drive.withVelocityX(0.5)
-        //             .withVelocityY(0)
-        //             .withRotationalRate(0)
-        //     )
-        //     .withTimeout(5.0),
-        //     // Finally idle for the rest of auton
-        //     drivetrain.applyRequest(() -> idle)
-        // );
         return new InstantCommand();
     }
 }
